31ec9cb5ee967e274f36dc82a653ef3767739cc7
Max <147649399+Zaroewn@users.noreply.github.com>
2024-01-19 15:03:25 +0100

    Update index.php

--- Atelier-PDO-fonctions/index.php
+++ Atelier-PDO-fonctions/index.php
@@ -2,19 +2,21 @@
 
 require_once __DIR__.'/functions.php';
 
+// Connection à la base de données
 $pdo = getPDO('mysql:host=localhost;dbname=blog', 'root', '');
 
+// Récupération de tout les posts présents sur la base de données
 $posts = getPostsWithCategories($pdo, (int) 1, (int) 20);
 
 
-// Fonction anonyme
+// Fonction anonyme (8ème point)
 
 $postsSansCategories = array_filter($posts, function($posts) {
     return empty($posts['category_name']);
 });
 
 
-// Fonction fléchées
+// Fonction fléchées (8ème point)
 $postsSansCategories = array_filter($posts, fn($posts) => empty($posts['category_name']));
 
 echo '<pre>';
